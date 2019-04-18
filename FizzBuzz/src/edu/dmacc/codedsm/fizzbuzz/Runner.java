da5ce5cab02a46edc80837ad21baf58ca324d9dc
Guru <grmadamsetty@dmacc.edu>
2019-04-18 18:55:53 -0500

    InitalCommit

---
+++ FizzBuzz/src/edu/dmacc/codedsm/fizzbuzz/Runner.java
@@ -0,0 +1,18 @@
+package edu.dmacc.codedsm.fizzbuzz;
+
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter User Name");
+		String userName = in.next();
+		
+		System.out.println("Enter Number");
+		int inputNumber = in.nextInt();
+		
+
+	}
+
+}
